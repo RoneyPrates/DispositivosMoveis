79eed7fb67945a65579fd09fb7924e85e1412de2
Roney <107938728+RoneyPrates@users.noreply.github.com>
2023-09-25 19:39:11 -0300

    Add files via upload

---
+++ LancarPedido.java
@@ -0,0 +1,14 @@
+package com.example.trabalhodeprogramacaoparadispositivosmoveis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LancarPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lancar_pedido);
+    }
+}
